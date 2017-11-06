7eb0387a155357d1cc6666fcc1168b596806583c
Korey Pace <kpace@tpnyc.local>
2017-11-06 14:05:33 +0100

    changed greeting

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 //express route handler
 app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+  res.send({ bye: 'buddy' });
 });
 
 const PORT = process.env.PORT || 5000; //runtime configuration on the port
