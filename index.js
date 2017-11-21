9a5b74d28e204a3c17c85edeaffed92ee2a7f6cb
Korey Pace <kpace@tpnyc.local>
2017-11-21 16:11:16 +0100

    added mongodb

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const mongoose = require('mongoose');
 const keys = require('./config/keys');
 
-mongoose.connect(keys.mongoURI);
-
+require('./models/User');
 require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
 
 const app = express();
 
--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 //function call creating a new instance of GoogleStrategy
 passport.use(
@@ -12,9 +15,7 @@ passport.use(
     },
     //identifying user information can now be save to the database
     (accessToken, refreshToken, profile, done) => {
-      console.log('access token', accessToken);
-      console.log('refresh token', refreshToken);
-      console.log('profile!', profile);
+      new User({ googleId: profile.id }).save(); //save the user's googleId to Mongo database
     }
   )
 );
