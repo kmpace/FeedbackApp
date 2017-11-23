903338231fc2dca67abd6f2a65216f78bacbd7f3
Korey Pace <kpace@tpnyc.local>
2017-11-23 17:07:19 +0100

    proxy fix

--- services/passport.js
+++ services/passport.js
@@ -23,7 +23,8 @@ passport.use(
     {
       clientID: keys.googleClientID,
       clientSecret: keys.googleClientSecret,
-      callbackURL: '/auth/google/callback'
+      callbackURL: '/auth/google/callback',
+      proxy: true
     },
     //identifying user information can now be save to the database
     (accessToken, refreshToken, profile, done) => {
